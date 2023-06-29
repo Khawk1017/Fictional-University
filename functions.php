6a36ca3126578795e806ece538ff50621776d461
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-06-29 12:08:34 -0700

    google script and filter

--- functions.php
+++ functions.php
@@ -32,6 +32,7 @@ function pageBanner($args = NULL) {
 
 function university_files() {
     // defined a new function named 'university_files' and within we are defining a WP function to point to the stylesheet we want to use 
+    wp_enqueue_script('googleMap', '//maps.googleapis.com/maps/api/js?key=AIzaSyCgeEliRFs2P7RB6prF7nMTR5MNmIHgU-U', NULL, '1.0', true);
     wp_enqueue_script('main-university-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style('custom-goolge-fonts',  '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome',  '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
