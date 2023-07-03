be15a0d3e84eeeed3a14318c63eb313226017c18
Kameron Hawk <yungkam@Kamerons-MacBook-Pro.local>
2023-07-02 20:31:20 -0700

    campus if statement query

--- functions.php
+++ functions.php
@@ -55,6 +55,10 @@ function univeristy_title() {
 add_action('after_setup_theme', 'univeristy_title');
 
 function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('campus') AND is_main_query()) {
+    $query->set('posts_per_page', -1);
+}
+
 
     if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
         $query->set('orderby', 'title');
